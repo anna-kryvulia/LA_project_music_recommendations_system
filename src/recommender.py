bec1887ef194a5080042bcec0d4c166b5ac7ef54
LidaSokha03 <sokha.pn@ucu.edu.ua>
2026-04-06 20:47:12 +0300

    manual svd

--- src/recommender.py
+++ src/recommender.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def recommend_top_n(
     original_matrix,
     predicted_matrix,
@@ -8,24 +7,21 @@ def recommend_top_n(
     index_to_artist: dict,
     n: int = 10
 ):
-    """
-    Recommend artists the user has not listened to yet.
-    """
     user_interactions = original_matrix[user_index].toarray().ravel()
     user_scores = predicted_matrix[user_index].copy()
 
-    # Remove already listened artists
     user_scores[user_interactions > 0] = -np.inf
 
     top_indices = np.argsort(user_scores)[::-1][:n]
 
     recommendations = [
         {
-            "Artist index": int(idx),
-            # "artist_id": index_to_artist[idx],
-            "Score": float(user_scores[idx]),
+            "artist_index": int(idx),
+            "artist_id": int(index_to_artist[idx]),
+            "score": float(user_scores[idx]),
         }
         for idx in top_indices
+        if np.isfinite(user_scores[idx])
     ]
 
     return recommendations
